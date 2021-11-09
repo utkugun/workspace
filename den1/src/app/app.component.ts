53a8ad7e60d628b1c1764c8c001392a389bab32a
utkugun <utkugun@gmail.com>
2021-11-09 09:14:43 +0000

    j

--- den1/src/app/app.component.ts
+++ den1/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'den1';
   ad:string="utku"
+  champ: any = [];
+  constructor(private api: ApiService) {}
+
+  getchamps() {
+    this.api.getir()
+      .subscribe(data => {
+        for (const d of (data as any)) {
+          this.champ.push({
+            name: d.data,
+         
+        }
+        console.log(this.champ);
+      });
+  }
+
+
 }
