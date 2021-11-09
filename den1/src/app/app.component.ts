26c3eaacd3c7f99d41a2eddf0bd30c315b6d7cd1
utkugun <utkugun@gmail.com>
2021-11-09 09:15:03 +0000

    k

--- den1/src/app/app.component.ts
+++ den1/src/app/app.component.ts
@@ -7,22 +7,16 @@ import { ApiService } from './api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'den1';
-  ad:string="utku"
+ 
   champ: any = [];
   constructor(private api: ApiService) {}
 
   getchamps() {
-    this.api.getir()
-      .subscribe(data => {
-        for (const d of (data as any)) {
-          this.champ.push({
-            name: d.data,
-         
-        }
-        console.log(this.champ);
-      });
+    this.api.getir().subscribe(data => {
+        for (const d of (data as any)) 
+        {this.champ.push(d.data)}
+        console.log(this.champ)
+    })
   }
-
 
 }
